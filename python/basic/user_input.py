c62eef29569f71c8e6fb04f34cb36b9c7deb50b2
kaironlucas <noriakxp@gmail.com>
2023-10-27 20:18:45 -0300

    debug user_input.py

---
+++ python/basic/user_input.py
@@ -0,0 +1,10 @@
+"""
+Esse código mostra como coletar informações do usuário e mostrar na tela
+"""
+
+# Coleta as informações
+name = input('Qual seu nome? ')
+age = input('Qual sua idade? ')
+
+# Mostrando na tela
+print(f'1 - Usuário: {name}  idade: {age}')
