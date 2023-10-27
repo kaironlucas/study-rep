c254903e3fb6cf4ec6e7c90fb706851718090deb
kaironlucas <noriakxp@gmail.com>
2023-10-27 16:58:57 -0300

    simple conditional

---
+++ python/basic/conditional-statment.py
@@ -0,0 +1,9 @@
+# Coleta as informações
+name = input('Qual seu nome? ')
+age = int(input('Qual sua idade? '))
+
+if age >= 18:
+    print(f'{name} é maior de idade.')
+
+else:
+    print(f'{name} menor de idade.')
