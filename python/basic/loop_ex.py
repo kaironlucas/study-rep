f5153ef90e89bb31b317f02e0cae091ba55998ec
kaironlucas <noriakxp@gmail.com>
2023-10-27 20:14:14 -0300

    debug loop code

---
+++ python/basic/loop_ex.py
@@ -0,0 +1,18 @@
+"""
+Código mostra exemplos de de laços de repetição
+"""
+
+fruits = ['Apple', 'Banana', 'Orange', 'Lemon']
+
+print('\nUsando For: ')
+for fruit in fruits:
+    print(fruit, end=" ")
+
+print('\n\nUsando While: ')
+CONT = 0
+
+while CONT < len(fruits):
+    print(fruits[CONT], end=" ")
+    CONT += 1
+
+print()
