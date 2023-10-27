cda7e7faa2e0f1439f48083d2f02e381bdf78707
kaironlucas <noriakxp@gmail.com>
2023-10-27 20:22:40 -0300

    debug conditional_statment.py

--- python/basic/conditional_st.py
+++ python/basic/conditional_st.py
@@ -1,9 +1,13 @@
-# Coleta as informações
+"""
+Esse código mostra como usar um simples controle de decisão
+"""
+
+# Coleta as informações do usuário
 name = input('Qual seu nome? ')
 age = int(input('Qual sua idade? '))
 
+# Controle de decisão
 if age >= 18:
     print(f'{name} é maior de idade.')
-
 else:
     print(f'{name} menor de idade.')
